ddd0b71a2832a45ca47c51aeefe47bb8d0665262
kesslern <nathant93@gmail.com>
2018-02-03 12:03:21 -0500

    Add handwired keyboards

---
+++ keyboards/kesslern_handwired_zealios/keymaps/default/keymap.c
@@ -0,0 +1,72 @@
+/* Copyright 2017 REPLACE_WITH_YOUR_NAME
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "kesslern_handwired_zealios.h"
+
+#define BASE 0
+#define AUX  1
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [BASE] = KEYMAP(
+               KC_GRAVE,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSPACE, \
+               KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_BSLASH, \
+               KC_LCTRL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_ENTER, \
+               KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_UP, \
+               MO(1), KC_LGUI, KC_LALT, KC_SPACE, MO(1), KC_LEFT, KC_DOWN, KC_RIGHT
+                ),
+  [AUX] = KEYMAP(
+               KC_ESC,  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, RESET, \
+               KC_TRNS,  KC_NO, KC_MPRV, KC_MNXT, KC_VOLU, KC_NO, KC_HOME, KC_PGUP, KC_UP, KC_PGDOWN, KC_NO, KC_NO, KC_NO, KC_NO, \
+               KC_TRNS, KC_NO, KC_NO, KC_MPLY, KC_VOLD, KC_NO, KC_END, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_NO, KC_NO, \
+               KC_TRNS, KC_NO, KC_NO, KC_NO, KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, \
+               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+                 ),
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+  [AUX] = ACTION_LAYER_TAP_TOGGLE(AUX)                // FN1 - Momentary Layer 1 (Aux)
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  // MACRODOWN only works in this function
+      switch(id) {
+        case 0:
+          if (record->event.pressed) {
+            register_code(KC_RSFT);
+          } else {
+            unregister_code(KC_RSFT);
+          }
+        break;
+      }
+    return MACRO_NONE;
+};
+
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
---
+++ keyboards/kesslern_handwired_zealios/keymaps/default/readme.md
@@ -0,0 +1,1 @@
+# The default keymap for kesslern_handwired_zealios
