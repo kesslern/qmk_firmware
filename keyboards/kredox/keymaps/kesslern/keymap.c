406e19a68eacb5bee13d29ad74b6aaf403ab72e5
kesslern <nathant93@gmail.com>
2019-10-13 19:55:32 -0400

    Update kredox keymap

--- keyboards/kredox/keymaps/kesslern/keymap.c
+++ keyboards/kredox/keymaps/kesslern/keymap.c
@@ -28,6 +28,8 @@
 #define SYM_I LT(SYMB, DV_I)
 #define SYM_D LT(SYMB, DV_D)
 
+#define NAV_E LT(NAV, DV_E)
+
 // Navigation layer / arrow keys
 #define NAVLEFT LT(NAV, KC_LEFT)
 #define NAVRGHT LT(NAV, KC_RGHT)
@@ -39,39 +41,80 @@
 // Alt + Win combination
 #define GUI_ALT(x) GUI_T(ALT_T((x)))
 
-#define WIN_LEFT GUI_T(KC_LEFT)
-#define CTL_BSPC CTL_T(KC_BSPC)
-#define ALT_DEL  ALT_T(KC_DEL)
-#define ALT_ENT  ALT_T(KC_ENT)
-#define CTL_SPC  CTL_T(KC_SPC)
-#define WIN_RGHT GUI_T(KC_RGHT)
+#define WN_UP GUI_T(KC_UP)
+#define CT_BSPC CTL_T(KC_BSPC)
+#define AL_DEL  ALT_T(KC_DEL)
+#define AL_ENT  ALT_T(KC_ENT)
+#define CT_SPC  CTL_T(KC_SPC)
+#define WN_DOWN GUI_T(KC_DOWN)
 
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes { NONE };
+/*
+        Template for a layer of the keymap. Copy/paste and customize.
+
+        [LAYER] = LAYOUT(
+              // XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_TRNS,
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,         KC_TRNS, KC_TRNS,
+
+                          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_TRNS, KC_TRNS, KC_NO,         KC_NO,      KC_NO,   KC_NO,   KC_NO
+        ),
+
+*/
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [BASE] = LAYOUT(
-                 KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  KC_ESC,                     DV_EQL,  KC_6,  KC_7,    KC_8,  KC_9,    KC_0,  KC_NO,
-                 KC_TILD, DV_QUOT, DV_COMM, DV_DOT,  DV_P,    DV_Y,  DV_BSLS,                    DV_SLSH, DV_F,  DV_G,    DV_C,  DV_R,    DV_L,  DV_SLSH,
-                 KC_TAB,  DV_A,    DV_O,    DV_E,    SH_DV_U, SYM_I,                                      SYM_D, SH_DV_H, DV_T,  DV_N,    DV_S,  DV_MINS,
-                 KC_LSFT, DV_SCLN, DV_Q,    DV_J,    DV_K,    DV_X,  KC_NO,    KC_H,    KC_A,    DV_E,    DV_B,  DV_M,    DV_W,  DV_V,    DV_Z,  KC_RSFT,
-                 MO(SYMB),KC_MPLY, KC_VOLD, KC_VOLU,    WIN_LEFT,    CTL_BSPC, ALT_DEL, ALT_ENT, CTL_SPC,    WIN_RGHT,    KC_UP, KC_DOWN, KC_NO, MO(SYMB)
-                 ),
+              // XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+                 KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_ESC,
+                 KC_TILD, DV_QUOT, DV_COMM, DV_DOT,  DV_P,    DV_Y,    DV_BSLS,
+                 KC_TAB,  DV_A,    DV_O,    NAV_E,   SH_DV_U, SYM_I,
+                 KC_LSFT, DV_SCLN, DV_Q,    DV_J,    DV_K,    DV_X,    XXXXXXX, KC_H,
+                 MO(SYMB),KC_MPLY, KC_VOLD, KC_VOLU,    WN_UP,         CT_BSPC, AL_DEL,
+
+                          KC_PSCR, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
+                          DV_EQL,  DV_F,    DV_G,    DV_C,    DV_R,    DV_L,    DV_SLSH,
+                                   SYM_D,   SH_DV_H, DV_T,    DV_N,    DV_S,    DV_MINS,
+                 KC_A,    DV_E,    DV_B,    DV_M,    DV_W,    DV_V,    DV_Z,    KC_RSFT,
+                 AL_ENT,  CT_SPC,     WN_DOWN,       TO(NAV), XXXXXXX, XXXXXXX, MO(SYMB)
+        ),
         [SYMB] = LAYOUT(
-                 KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G,             RESET, KC_G,    KC_C,    KC_1, KC_E, KC_F, KC_G,
-                 KC_A, KC_B, KC_C, KC_D, KC_H, KC_J, KC_K,             KC_D,  KC_NO,   KC_LPRN, KC_RPRN, KC_2, KC_E, KC_F,
-                 KC_A, KC_B, KC_C, KC_A, KC_B, KC_C,                          KC_TRNS, DV_LCBR, DV_RCBR, KC_E, KC_F, KC_G,
-                 KC_A, KC_B, KC_C, KC_L, KC_M, KC_A, KC_O, KC_H, KC_A, KC_E,  KC_J,    DV_LBRC, DV_RBRC, KC_E, KC_F, KC_G,
-                 KC_A, KC_B, KC_C, KC_D, KC_E,       KC_G, KC_H, KC_B, KC_F,   KC_C,  KC_5, KC_E, KC_F, KC_G
-                 ),
+              // XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+                 KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_INS,
+                 KC_NO,   KC_NO,   KC_EXLM, KC_AT,   KC_HASH, KC_NO,   KC_SLSH,
+                 KC_NO,   KC_NO,   KC_DLR,  KC_PERC, KC_PIPE, KC_TRNS,
+                 KC_TRNS, KC_NO,   KC_CIRC, KC_AMPR, KC_ASTR, KC_NO,   KC_NO,   KC_NO,
+                 KC_TRNS, KC_NO,   KC_NO,   KC_NO,      KC_LEFT,       KC_SPC,  KC_BSPC,
+
+                          KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+                          KC_NO,   KC_PIPE, KC_LPRN, KC_RPRN, KC_NO,   KC_NO,   KC_F12,
+                                   KC_TRNS, DV_LCBR, DV_RCBR, KC_NO,   KC_NO,   KC_NO,
+                 RESET,   KC_NO,   KC_PLUS, DV_LBRC, DV_RBRC, KC_NO,   KC_NO,   KC_TRNS,
+                 KC_NO,   KC_NO,      KC_RGHT,       KC_NO,   KC_NO,   KC_NO,   KC_TRNS
+        ),
         [NAV] = LAYOUT(
-                 KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G,             KC_C, KC_G, KC_C, KC_1, KC_E, KC_F, KC_G,
-                 KC_A, KC_B, KC_C, KC_D, KC_H, KC_J, KC_K,             KC_D, KC_H, KC_C, KC_2, KC_E, KC_F, KC_G,
-                 KC_A, KC_B, KC_C, KC_A, KC_B, KC_C,                         KC_I, KC_C, KC_3, KC_E, KC_F, KC_G,
-                 KC_A, KC_B, KC_C, KC_L, KC_M, KC_A, KC_O, KC_H, KC_A, KC_E, KC_J, KC_C, KC_4, KC_E, KC_F, KC_G,
-                 KC_A, KC_B, KC_C, KC_D, KC_E,       KC_G, KC_H, KC_B, KC_F,       KC_C, KC_5, KC_E, KC_F, KC_G
-                 ),
+              // XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+                 RESET,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO,   
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,         KC_NO,   KC_NO,
+
+                          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                          KC_NO,   KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_NO,
+                                   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_NO,   KC_NO,      KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO
+        ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
