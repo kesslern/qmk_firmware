85c6deaa5377bd201df96697b8de332c700044fd
kesslern <nathant93@gmail.com>
2019-10-31 20:59:17 -0400

    Update nav access; add make & flash commands; add dynamic layout

--- keyboards/ergodox_ez/keymaps/kesslern/keymap.c
+++ keyboards/ergodox_ez/keymaps/kesslern/keymap.c
@@ -3,6 +3,7 @@
 #include "action_layer.h"
 #include "version.h"
 #include "keymap_dvorak.h"
+#include <sendstring_dvorak.h>
 
 #define BASE 0
 #define SYMB 1
@@ -20,6 +21,8 @@
 #define NAVLEFT LT(NAV, KC_LEFT)
 #define NAVRGHT LT(NAV, KC_RGHT)
 
+#define NAV_E LT(NAV, DV_E)
+
 // Ctrl+Shift+C and Ctrl+Shift+V
 #define SH_CT_C LCTL(LSFT(DV_C))
 #define SH_CT_V LCTL(LSFT(DV_V))
@@ -27,12 +30,22 @@
 // Alt + Win combination
 #define GUI_ALT(x) GUI_T(ALT_T((x)))
 
+#define WN_UP GUI_T(KC_UP)
+#define WN_DOWN GUI_T(KC_DOWN)
+
+#define MACRO_1 DYN_MACRO_PLAY1
+#define MACRO_2 DYN_MACRO_PLAY2
+
+#define RCMAC_1 DYN_REC_START1
+#define RCMAC_2 DYN_REC_START2
+
 enum custom_keycodes {
-  PLACEHOLDER = SAFE_RANGE, // can always be here
-  EPRM,
-  VRSN,
-  RGB_SLD
+  KC_MAKE = SAFE_RANGE, // can always be here
+  KC_FLSH,
+  DYNAMIC_MACRO_RANGE,
 };
+
+#include "dynamic_macro.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -40,18 +53,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_GRV,   DV_1,    DV_2,    DV_3,    DV_4,    DV_5,  KC_ESC,
         KC_TILD,  DV_QUOT, DV_COMM, DV_DOT,  DV_P,    DV_Y,  DV_BSLS,
-        KC_TAB,   DV_A,    DV_O,    DV_E,    SH_DV_U, SYM_I,
+        KC_TAB,   DV_A,    DV_O,    NAV_E,   SH_DV_U, SYM_I,
         KC_LSFT,  DV_SCLN, DV_Q,    DV_J,    DV_K,    DV_X,  KC_PIPE,
-        MO(SYMB), KC_MPLY, KC_VOLD, KC_VOLU, NAVLEFT,
-                                                   KC_HOME,  KC_END,
-                                                     GUI_ALT(KC_PGUP),
-                              CTL_T(KC_BSPC), ALT_T(KC_DEL), GUI_T(KC_PGDN),
+        MO(SYMB), KC_MPLY, KC_VOLD, KC_VOLU, WN_UP,
+                                                   MACRO_1,  MACRO_2,
+                                                    GUI_ALT(KC_PGUP),
+                       CTL_T(KC_BSPC), ALT_T(KC_DEL), GUI_T(KC_PGDN),
         // right hand
         KC_NO,   DV_6,    DV_7,    DV_8,  DV_9,    DV_0,    KC_NO,
         DV_SLSH, DV_F,    DV_G,    DV_C,  DV_R,    DV_L,    DV_SLSH,
                  SYM_D,   SH_DV_H, DV_T,  DV_N,    DV_S,    DV_MINS,
         DV_EQL,  DV_B,    DV_M,    DV_W,  DV_V,    DV_Z,    KC_RSFT,
-                          NAVRGHT, KC_UP, SH_CT_C, SH_CT_V, MO(SYMB),
+                          WN_DOWN, KC_UP, SH_CT_C, SH_CT_V, MO(SYMB),
         KC_LEFT, KC_RGHT,
         GUI_ALT(KC_UP),
         GUI_T(KC_DOWN), ALT_T(KC_ENT), CTL_T(KC_SPC)
@@ -63,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TRNS, KC_NO, KC_NO,   KC_DLR,  KC_PERC, KC_TRNS,
          KC_TRNS, KC_NO, KC_NO,   KC_CIRC, KC_AMPR, KC_ASTR, KC_NO,
          KC_TRNS, KC_NO, KC_NO,   KC_NO,   KC_NO,
-                                                    KC_TRNS, KC_TRNS,
+                                                    RCMAC_1, RCMAC_2,
                                                              KC_TRNS,
                                CTL_T(KC_SPC), ALT_T(KC_ENT), KC_TRNS,
          // right hand
@@ -91,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_NO, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_NO, KC_NO,
                 KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO,
          KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
-                KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                KC_NO,   KC_NO,   KC_NO,   KC_MAKE, KC_FLSH,
          KC_TRNS, KC_TRNS,
          KC_TRNS,
          KC_TRNS, KC_TRNS, KC_TRNS
@@ -108,6 +121,24 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_record_dynamic_macro(keycode, record)) {
+    return false;
+  }
+  
+   switch (keycode) {
+    case KC_MAKE:
+      if (record->event.pressed) {
+        SEND_STRING("make ergodox_ez:kesslern"SS_TAP(X_ENTER));
+      }
+      break;
+   case KC_FLSH:
+      if (record->event.pressed) {
+        SEND_STRING("sleep 2s && sudo teensy-loader-cli -mmcu=atmega32u4 ergodox_ez_kesslern.hex"SS_TAP(X_ENTER));
+        reset_keyboard();
+      }
+      break;
+   }
+  
   return true;
 }
 
