d40f914981fd1a0aeedf524e69a8004d7e06bff7
kesslern <nathant93@gmail.com>
2020-02-13 19:23:04 -0500

    Fix katana60 keymap

--- keyboards/katana60/keymaps/kesslern/keymap.c
+++ keyboards/katana60/keymaps/kesslern/keymap.c
@@ -22,14 +22,14 @@
 #define SYMB 1 // Symbols
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[BASE] = KEYMAP( /* Base */
+[BASE] = LAYOUT( /* Base */
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MPLY, DF(1),   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  DV_LBRC, DV_RBRC,
   KC_TAB,  DV_QUOT, DV_COMM, DV_DOT,  DV_P,    DV_Y,    DV_BSLS,          DV_EQL,  DV_F,    DV_G,    DV_C,    DV_R,  DV_L,    DV_SLSH,
   KC_LCTL, DV_A,    DV_O,    DV_E,    DV_U,    DV_I,    DV_EQL,           KC_ENT,  DV_D,    DV_H,    DV_T,    DV_N,  DV_S,    DV_MINS,
   KC_LSFT, DV_SCLN, DV_Q,    DV_J,    DV_K,    DV_X,    KC_HOME, KC_LALT, KC_END,  DV_B,    DV_M,    DV_W,    DV_V,  DV_Z,    KC_RSFT,
   MO(1),   XXXXXXX, KC_LGUI, KC_LALT,          CTL_T(KC_BSPC), KC_ENT,  KC_SPC,      ALT_T(KC_LEFT), KC_DOWN, KC_UP, KC_RGHT, MO(1)
     ),
-[SYMB] = KEYMAP(
+[SYMB] = LAYOUT(
   KC_ESC,  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,          _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_VOLD,          KC_VOLU, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, RESET,
