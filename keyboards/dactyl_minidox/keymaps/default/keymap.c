92735920577f971371065ed940914cd63785749c
Nathan Kessler <nathant93@gmail.com>
2020-06-17 20:45:30 -0400

    Update layout

--- keyboards/dactyl_minidox/keymaps/default/keymap.c
+++ keyboards/dactyl_minidox/keymaps/default/keymap.c
@@ -46,20 +46,20 @@ enum custom_keycodes {
 
 #define TABBETA LT(_BETA, KC_TAB)
 #define ENTRF09 LT(_NUMFN, KC_ENT)
-#define I_F09   LT(_NUMFN, DV_I)
-#define D_F09   LT(_NUMFN, DV_D)
+#define BETA_I  LT(_BETA, DV_I)
+#define BETA_D  LT(_BETA, DV_D)
 #define SH_SPC  SFT_T(KC_SPC)
 
 #define NAV_E   LT(_NAV, DV_E)
 
-#define BETA_K  LT(_BETA, DV_K)
+#define F09_K  LT(_NUMFN, DV_K)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
                      DV_QUOT, DV_COMM, DV_DOT,  DV_P,    DV_Y,    DV_F,    DV_G,    DV_C,    DV_R,    DV_L,    \
-                     SH_DV_A, DV_O,    NAV_E,   SH_DV_U, I_F09,   D_F09,   SH_DV_H, DV_T,    DV_N,    SH_DV_S, \
-                     DV_SCLN, DV_Q,    DV_J,    BETA_K,  DV_X,    DV_B,    DV_M,    DV_W,    DV_V,    DV_Z,    \
+                     SH_DV_A, DV_O,    NAV_E,   SH_DV_U, BETA_I,  BETA_D,  SH_DV_H, DV_T,    DV_N,    SH_DV_S, \
+                     DV_SCLN, DV_Q,    DV_J,    F09_K,   DV_X,    DV_B,    DV_M,    DV_W,    DV_V,    DV_Z,    \
                                        CTLBSPC, ALT_DEL, WINSLSH, TABBETA, ENTRF09, SH_SPC                     \
                      ),
     [_NUMFN] = LAYOUT(
@@ -69,8 +69,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS                    \
                      ),
     [_BETA] = LAYOUT(
-                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   DV_EQL,  KC_LPRN, KC_RPRN, KC_TILD, KC_PIPE, \
-                     KC_TRNS, KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, DV_PLUS, SH_LCBR, DV_RCBR, DV_MINS, DV_UNDS, \
+                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   DV_PLUS, KC_LPRN, KC_RPRN, KC_TILD, KC_PIPE, \
+                     KC_TRNS, KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, DV_EQL,  DV_LCBR, DV_RCBR, DV_MINS, DV_UNDS, \
                      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_ESC,  KC_BSLS, DV_LBRC, DV_RBRC, DV_SLSH, DV_EQL,  \
                                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS                    \
                      ),
